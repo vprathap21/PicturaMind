a234fccf75d68d34918c348ce305902e9972bf7f
prathap v <vprathap585@gmail.com>
2024-10-16 19:16:43 +0530

    Update route.ts

--- app/api/generateImage/route.ts
+++ app/api/generateImage/route.ts
@@ -11,7 +11,7 @@ if (process.env.UPSTASH_REDIS_REST_URL) {
   ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
     // Allow 100 requests per day (~5-10 prompts)
-    limiter: Ratelimit.fixedWindow(100, "1440 m"),
+    limiter: Ratelimit.fixedWindow(20, "1440 m"),
     analytics: true,
     prefix: "picturnmind",
   });
